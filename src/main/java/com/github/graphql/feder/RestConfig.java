5707ab5e26c676847311f296bdd8523e0e37ca41
Rüdiger zu Dohna <snackbox@sinntr.eu>
2021-07-18 08:26:34 +0200

    initial code

---
+++ src/main/java/com/github/graphql/feder/RestConfig.java
@@ -0,0 +1,7 @@
+package com.github.graphql.feder;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestConfig extends Application {}
